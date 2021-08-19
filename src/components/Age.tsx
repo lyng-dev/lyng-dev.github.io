843148879407276074c9b1f104945e55d9f07985
lyng-dev <sm@lyng.dev>
2021-08-20 00:03:23 +0200

    refactor

--- src/components/Age.tsx
+++ src/components/Age.tsx
@@ -1,22 +1,5 @@
 import { useEffect, useState } from "react";
 
-export const Age = () => {
-  const [age, setAge] = useState("Calculating..");
-  useEffect(() => {
-    fetch("https://worldtimeapi.org/api/timezone/CET")
-      .then((response) => response.json())
-      .then((data) =>
-        delaySlightlyForSuspense(() => setAge(getYearsOfAge(data.datetime)))
-      );
-  }, []);
-
-  return (
-    <a href="https://github.com/lyng-dev/lyng-dev.github.io/blob/main/src/components/Age.tsx">
-      <span>{age}</span>
-    </a>
-  );
-};
-
 const getYearsOfAge = (currentDate: string): string => {
   const birthday = "1979-08-14";
   const dateTo = new Date(currentDate);
@@ -32,4 +15,21 @@ const delaySlightlyForSuspense = (callback: Function) => {
   setTimeout(function () {
     callback();
   }, 1 + Math.floor(Math.random() * 1000 * 2));
+};
+
+export const Age = () => {
+  const [age, setAge] = useState("Calculating..");
+  useEffect(() => {
+    fetch("https://worldtimeapi.org/api/timezone/CET")
+      .then((response) => response.json())
+      .then((data) =>
+        delaySlightlyForSuspense(() => setAge(getYearsOfAge(data.datetime)))
+      );
+  }, []);
+
+  return (
+    <a href="https://github.com/lyng-dev/lyng-dev.github.io/blob/main/src/components/Age.tsx">
+      <span>{age}</span>
+    </a>
+  );
 };
